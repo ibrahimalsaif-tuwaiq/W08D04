0365d4080e622dee9e8269d5b2f021e91890f547
ibrahimalsaif-tuwaiq <Ibrahim.Husain.elq.js@tuwaiq.edu.sa>
2021-12-01 12:02:18 +0300

    create role controller

--- routers/controllers/roles.js
+++ routers/controllers/roles.js
@@ -5,7 +5,21 @@ const getRoles = (req, res) => {
 };
 
 const createRole = (req, res) => {
-  // code
+  const { role, permissions } = req.body;
+
+  const newRole = new rolesModel({
+    role,
+    permissions,
+  });
+
+  newRole
+    .save()
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 };
 
 module.exports = { getRoles, createRole };

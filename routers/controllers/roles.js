b84e0e0dfe946147b2a1a9dfa292bd585184cd2a
ibrahimalsaif-tuwaiq <Ibrahim.Husain.elq.js@tuwaiq.edu.sa>
2021-12-01 12:04:15 +0300

    get roles controller

--- routers/controllers/roles.js
+++ routers/controllers/roles.js
@@ -1,7 +1,14 @@
 const rolesModel = require("./../../db/models/roles");
 
 const getRoles = (req, res) => {
-  // code
+  rolesModel
+    .find({})
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 };
 
 const createRole = (req, res) => {

ddf248c4804377d42c14a19a6cfad115351b4cc5
ibrahimalsaif-tuwaiq <Ibrahim.Husain.elq.js@tuwaiq.edu.sa>
2021-12-01 13:38:28 +0300

    login controller

--- routers/controllers/users.js
+++ routers/controllers/users.js
@@ -9,15 +9,19 @@ dotenv.config();
 // Get SALT variable from .env
 const SALT = Number(process.env.SALT);
 
+// Get SECRET_KEY variable from .env
+const SECRET = process.env.SECRET_KEY;
+
 const signup = async (req, res) => {
   const { email, username, password, avatar, role } = req.body;
 
   const lowerCaseEmail = email.toLowerCase();
+  const lowerCaseUsername = username.toLowerCase();
   const hashedPassword = await bcrypt.hash(password, SALT);
 
   const newUser = new usersModel({
     email: lowerCaseEmail,
-    username,
+    username: lowerCaseUsername,
     password: hashedPassword,
     avatar,
     role,
@@ -34,7 +38,63 @@ const signup = async (req, res) => {
 };
 
 const login = (req, res) => {
-  // code
+  const { identifier, password } = req.body;
+
+  const lowerCaseIdentifier = identifier.toLowerCase();
+
+  usersModel
+    .findOne({
+      $or: [{ username: identifier }, { email: identifier }],
+    })
+    .populate("role")
+    .then(async (result) => {
+      if (result) {
+        if (result.deleted === false) {
+          if (
+            result.email == lowerCaseIdentifier ||
+            result.username == lowerCaseIdentifier
+          ) {
+            const matchedPassword = await bcrypt.compare(
+              password,
+              result.password
+            );
+
+            if (matchedPassword) {
+              const payload = {
+                id: result._id,
+                email: result.email,
+                username: result.username,
+                role: result.role.role,
+                deleted: result.deleted,
+              };
+
+              const options = {
+                expiresIn: "60m",
+              };
+
+              const token = jwt.sign(payload, SECRET, options);
+
+              res.status(200).json({ result, token });
+            } else {
+              res
+                .status(400)
+                .json({ message: "Invalid Email/Username or Password!!" });
+            }
+          } else {
+            res
+              .status(400)
+              .json({ message: "Invalid Email/Username or Password!!" });
+          }
+        } else {
+          res.status(404).json({ message: "This user is deactivated you can't login!!" });
+        }
+      } else {
+        res.status(404).json({ message: "Email/Username does not exist!!" });
+      }
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 };
 
 const getUsers = (req, res) => {

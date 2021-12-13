3032a2fa3ef0eff2ed49bacfcee9192ec790b889
ibrahimalsaif-tuwaiq <Ibrahim.Husain.elq.js@tuwaiq.edu.sa>
2021-12-13 22:45:41 +0300

    fix google callback url

--- Config/passport.js
+++ Config/passport.js
@@ -16,7 +16,7 @@ passport.use(
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL:
-        "https://ibrahim-social-media-project.netlify.app/auth/google/callback",
+        "https://social-media-project-w08d04.herokuapp.com/auth/google/callback",
       passReqToCallback: true,
     },
     async (request, accessToken, refreshToken, profile, done) => {
